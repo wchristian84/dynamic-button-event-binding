635f5d24aa9ea0809463c4dad36f346236838017
wchristian84 <wade.christian@gmail.com>
2022-09-26 20:42:51 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dynamic-button-event-binding';
+  isDisabled = true;
+  disabledButton = "Currently disabled";
+  enabledButton = "Currently enabled";
 }
